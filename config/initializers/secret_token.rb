7310698633f45b7647636aa3e58711898c8840ed
Kent Nagata <ngtknt@gmail.com>
2013-08-26 09:18:12 +0900

    New Rails rails-resque-mock

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RailsResqueMock::Application.config.secret_key_base = '66d61991aad77211888a246d0cd5019cb62a78cefde1152429b5b8e7bfbae60c272b9df381adea35d6ea69135049fdf34ebb10df9eb7121f2d06fa4917a3f6e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RailsResqueMock::Application.config.session_store :cookie_store, key: '_rails-resque-mock_session'
