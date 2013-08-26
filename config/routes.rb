f5ce67f41a7087399d4b14f7b928eaca8fd24ed6
Kent Nagata <ngtknt@gmail.com>
2013-08-26 09:26:21 +0900

    create resque-initializer and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsResqueMock::Application.routes.draw do
+  get 'hello/:text', :to => 'hello#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
